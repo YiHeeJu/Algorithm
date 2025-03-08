4ce8521ddf17adf7bd38201824e05868ee89f309
Yi HeeJu <yiheeju0330@gmail.com>
2025-03-08 18:41:38 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,36 +1,16 @@
-count = 1
+N = int(input())
+a = N//10 
+b = N%10
+i = 0
 
-def num_count(x):
-    if len(str(x)) == 1:
-        num = x
-    elif len(str(x)) == 2:
-        num = [int(i) for i in str(x)]
-        num = num[1]
-    return num
+while True:
+    c = b*10 + (a+b)%10
+    a = c//10
+    b = c%10
+    i+=1
+    if c==N:
+        break
+    else:
+        continue
 
-
-n = list(map(int, input()))
-if len(n) == 1:
-    x = n[0]
-    n = [0, x]
-    
-
-m=[0, 0]
-m[0], m[1] = n[0], n[1]
-n1 = n[1]
-n2 = n[0] + n[1]
-m[0] = n1
-m[1] = n2
-
-time = 0
-while time != 1:
-    if n != m:
-        count += 1
-        n1 = m[1]
-        n2 = m[0] + m[1]
-        n2 = num_count(n2)
-        m[0] = n1
-        m[1] = n2
-    elif n == m :
-        time = 1
-        print(count)
+print(i)
